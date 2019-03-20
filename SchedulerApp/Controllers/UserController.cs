cc5f97daf88b4edc8320d346f362b427499a41ac
tilottj07 <tilottj@gmail.com>
2019-03-20 06:29:43 -0500

    datatables added from nuget

---
+++ SchedulerApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DataTables;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SchedulerApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
