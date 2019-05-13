8c0e44bee708b9656b5fc4bc363e513b1407d629
tilottj07 <tilottj@gmail.com>
2019-05-13 06:35:55 -0500

    warranty edit page in progress

--- SchedulerApp/Controllers/WarrantyController.cs
+++ SchedulerApp/Controllers/WarrantyController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Scheduler.BL.Schedule.Dto;
 using Scheduler.BL.Schedule.Implementation;
 using Scheduler.BL.Schedule.Interface;
 using Scheduler.BL.Schedule.Interface.Models;
 using Scheduler.BL.Shared;
 using Scheduler.BL.Team.Implementation;
 using Scheduler.BL.Team.Interface;
+using Scheduler.BL.User.Implementation;
+using Scheduler.BL.User.Interface;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,11 +22,13 @@ namespace SchedulerApp.Controllers
     {
         private IWarrantyService WarrantyService;
         private ITeamService TeamService;
+        private IUserService UserService;
 
         public WarrantyController()
         {
             WarrantyService = new WarrantyService();
             TeamService = new TeamService();
+            UserService = new UserService();
         }
 
         public IActionResult Index()
@@ -55,9 +61,52 @@ namespace SchedulerApp.Controllers
             Guid? warrantyId = Helper.ConvertToGuid(id);
             var teams = TeamService.GetTeams();
 
-            Models.Warranty.EditWarranty model = new Models.Warranty.EditWarranty(teams);
+            Models.Warranty.EditWarranty model;
+            if (warrantyId.HasValue)
+                model = new Models.Warranty.EditWarranty(WarrantyService.GetWarranty(warrantyId.Value), teams);
+            else
+                model = new Models.Warranty.EditWarranty(teams);
 
             return PartialView("_WarrantyEditPartial", model);
+        }
+
+        [HttpPost]
+        public IActionResult EditWarrantyModal(Models.Warranty.EditWarranty model)
+        {
+            if (ModelState.IsValid)
+            {
+                WarrantyDto dto = new WarrantyDto
+                {
+                    WarrantyId = model.WarrantyId,
+                    EndDate = model.EndDate.ToUniversalTime(),
+                    StartDate = model.StartDate.ToUniversalTime(),
+                    TeamId = model.TeamId,
+                    UserId = model.UserId,
+                    WarrantyName = model.WarrantyName,
+                    WarrentyDescription = model.WarrentyDescription
+                };
+
+                if (model.IsAddNew)
+                    model.Result = WarrantyService.AddWarranty(dto);
+                else
+                    model.Result = WarrantyService.UpdateWarranty(dto);
+            }
+            else
+            {
+                model.FillTeamsSelectList(TeamService.GetTeams());
+                model.UserSelectList = new List<SelectListItem>();
+            }
+
+            return PartialView("_WarrantyEditPartial", model);
+        }
+
+        public IActionResult GetTeamUsersSelectList(Guid teamId, Guid? userId)
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            foreach (var user in UserService.GetTeamUsers(teamId).OrderBy(x => x.LastName).ThenBy(x => x.FirstName))
+                list.Add(new SelectListItem { Selected = userId == user.UserId, Text = user.DisplayName, Value = user.UserId.ToString() });
+
+            return Json(list);
         }
     }
 }
--- SchedulerApp/Models/Warranty/EditWarranty.cs
+++ SchedulerApp/Models/Warranty/EditWarranty.cs
@@ -4,18 +4,20 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Scheduler.BL.Schedule.Interface.Models;
+using Scheduler.BL.Shared.Models;
 using Scheduler.BL.Team.Interface.Models;
 
 namespace SchedulerApp.Models.Warranty
 {
     public class EditWarranty
     {
-        public EditWarranty() { }
+        public EditWarranty() { IsAddNew = true; }
 
         public EditWarranty(List<ITeam> teams)
         {
             FillTeamsSelectList(teams);
             UserSelectList = new List<SelectListItem>();
+            IsAddNew = true;
         }
 
         public EditWarranty(IWarranty warranty, List<ITeam> teams)
@@ -29,26 +31,46 @@ namespace SchedulerApp.Models.Warranty
                 UserId = warranty.UserId;
                 StartDate = warranty.StartDate.ToLocalTime();
                 EndDate = warranty.EndDate.ToLocalTime();
+                UserIdPlaceholder = warranty.UserId;
             }
 
             FillTeamsSelectList(teams);
             UserSelectList = new List<SelectListItem>();
+            IsAddNew = false;
         }
 
         public Guid WarrantyId { get; set; }
 
         [Required]
         [Display(Name = "Warranty Name")]
+        [StringLength(100)]
         public string WarrantyName { get; set; }
 
         [Required]
         [Display(Name = "Warranty Description")]
+        [StringLength(500)]
         public string WarrentyDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Team")]
         public Guid TeamId { get; set; }
+
+        [Required]
+        [Display(Name = "User")]
         public Guid UserId { get; set; }
 
+        [Required]
+        [Display(Name = "Start")]
+        [DataType(DataType.DateTime)]
         public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End")]
+        [DataType(DataType.DateTime)]
         public DateTime EndDate { get; set; }
+
+        public bool IsAddNew { get; set; }
+        public Guid UserIdPlaceholder { get; set; }
 
 
 
@@ -56,12 +78,24 @@ namespace SchedulerApp.Models.Warranty
         public List<SelectListItem> UserSelectList { get; set; }
 
 
-        private void FillTeamsSelectList(List<ITeam> teams)
+        public void FillTeamsSelectList(List<ITeam> teams)
         {
             TeamSelectList = new List<SelectListItem>();
 
             foreach (var item in teams.OrderBy(x => x.TeamName))
                 TeamSelectList.Add(new SelectListItem { Selected = item.TeamId == TeamId, Text = item.TeamName, Value = item.TeamId.ToString() });
+        }
+
+
+        public ChangeResult Result { get; set; }
+
+        public string ModalTitle
+        {
+            get
+            {
+                if (IsAddNew) return "Add Warranty";
+                return "Update Warranty";
+            }
         }
     }
 }
